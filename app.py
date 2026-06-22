6b1b4032452f8743132fbe29856c5ed035153ee6
Xiaozhan114514 <133721053+Xiaozhan114514@users.noreply.github.com>
2026-06-21 20:50:13 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -169,5 +169,5 @@ demo = gr.Interface(
 )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 10000))
     demo.launch(server_name="0.0.0.0", server_port=port, ssr_mode=False)
